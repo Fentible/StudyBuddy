fde889b0d1214b58235cff6bc69ed08bcb2d14a1
Fentible <you@example.com>
2021-03-08 15:02:38 +0000

    model class base - T3

--- src/com/company/SemesterProfile.java
+++ src/com/company/SemesterProfile.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.List;
+
 public class SemesterProfile {
+
+    private List<Task> tasks;
+    // List<Reminder>
+    private List<Deadline> deadlines;
+    private List<Activity> activities;
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public List<Deadline> getDeadlines() {
+        return this.deadlines;
+    }
+
+    public List<Activity> getActivities() {
+        return this.activities;
+    }
+
 }
