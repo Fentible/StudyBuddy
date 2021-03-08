13b9ed25d518dc9f901b0c6197579c5b68aa6fbf
Fentible <you@example.com>
2021-03-08 16:35:43 +0000

    basic setup for model classes

--- src/com/company/model/Exam.java
+++ src/com/company/model/Exam.java
@@ -1,4 +1,12 @@
 package com.company.model;
 
+import java.time.LocalDateTime;
+
+/*
+ * Loaded in from the semester file
+ * not described much on our documentation
+ */
 public class Exam {
+
+    LocalDateTime dueDate;
 }
