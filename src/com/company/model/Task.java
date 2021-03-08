e18d78c0f365cddfabb08fd1454d3ae263ebcea7
Fentible <you@example.com>
2021-03-08 16:03:32 +0000

    basic setup for model classes

--- src/com/company/model/Task.java
+++ src/com/company/model/Task.java
@@ -1,5 +1,24 @@
 package com.company.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Task {
+
+    // 'Generic' attributes
+    private String title;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private int progress;
+    private String notes;
+    // add Reminder
+
+    // Related package classes
+    private Exam exam;
+    private Assignment assignment;
+    private Module module;
+    private List<Task> dependencies;
+    private List<Milestone> milestones;
+
 
 }
