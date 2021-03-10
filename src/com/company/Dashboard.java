f7f51a65ca7a6f7d94f43e2809451c8234426dcf
Fentible <you@example.com>
2021-03-10 21:58:25 +0000

    reconfigured and fixed issues with JavaFX

---
+++ src/com/company/Dashboard.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+public class Dashboard extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
